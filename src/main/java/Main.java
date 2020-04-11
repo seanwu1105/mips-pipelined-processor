41937739b386f775a0763d14b3a82c775748c489
seanwu1105 <seanwu1105@gmail.com>
2020-04-12 00:54:22 +0800

    Add test supports with JUnit 5.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Main.main");
     }
 
-    public static int myFunction(int para) {
-        return para;
+    public static int myFunction() {
+        return 1;
     }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void myFunction() {
+        assertEquals(1, Main.myFunction());
+    }
+}
