a004faec474c19a46b9f3b0b87ab9d8a54a3b844
Sean Wu <seanwu1105@gmail.com>
2020-04-25 16:44:50 +0800

    Add program counter.

---
+++ src/main/java/component/ProgramCounter.java
@@ -0,0 +1,14 @@
+package component;
+
+public class ProgramCounter {
+
+    private int counter = 0;
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
+}
