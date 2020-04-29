124d5675faf42b8558b3603463743bdf4b92c46e
Sean Wu <seanwu1105@gmail.com>
2020-04-29 16:25:17 +0800

    Implement InstructionFetch class.

--- src/main/java/component/pipeline/InstructionFetch.java
+++ src/main/java/component/pipeline/InstructionFetch.java
@@ -1,21 +1,40 @@
 package component.pipeline;
 
+import component.Memory;
+import controller.MainController;
 import org.jetbrains.annotations.NotNull;
 import signal.Instruction;
 
 public class InstructionFetch implements Stage {
 
+    @NotNull
+    final private Memory instructionMemory;
+
+    private int programCounter = 0;
+
+    @NotNull
+    private Instruction currentInstruction;
+
+    public InstructionFetch(@NotNull Memory instructionMemory) {
+        this.instructionMemory = instructionMemory;
+        this.instructionMemory.setMemoryRead(MainController.MemoryRead.TRUE);
+        this.instructionMemory.setAddress(programCounter);
+        currentInstruction = this.instructionMemory.readInstruction();
+    }
+
     public int getProgramCounter() {
-        return 0;
+        return programCounter;
     }
 
     @NotNull
     public Instruction getInstruction() {
-        return new Instruction("00000000000000000000000000000000");
+        return currentInstruction;
     }
 
     @Override
     public void run() {
-
+        instructionMemory.setAddress(programCounter);
+        currentInstruction = instructionMemory.readInstruction();
+        programCounter += 4;
     }
 }
--- src/test/java/component/StageTest.java
+++ src/test/java/component/StageTest.java
@@ -1,11 +1,52 @@
 package component;
 
+import component.pipeline.InstructionFetch;
+import controller.MainController;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import signal.Instruction;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StageTest {
 
+    @NotNull
+    private Memory instructionMemory;
+
+    @BeforeEach
+    void buildUp() {
+        instructionMemory = new Memory();
+        instructionMemory.setMemoryWrite(MainController.MemoryWrite.TRUE);
+    }
+
     @Test
-    void test() {}
+    void testInstructionFetchRun() {
+        int initProgramCounter = 0, number = 0;
+        Instruction firstInstruction = new Instruction("10001101000000010000000000000011");
+        Instruction secondInstruction = new Instruction("00000000000000100001100000100000");
+        List<Instruction> instructions = List.of(firstInstruction, secondInstruction);
+
+        for (Instruction instruction : instructions) {
+            instructionMemory.setAddress(initProgramCounter + number * 4);
+            instructionMemory.write(instruction);
+            number++;
+        }
+
+        InstructionFetch instructionFetch = new InstructionFetch(instructionMemory);
+
+        number = 0;
+        for (Instruction instruction : instructions) {
+            instructionFetch.run();
+            assertEquals((number + 1) * 4, instructionFetch.getProgramCounter());
+            assertEquals(instruction, instructionFetch.getInstruction());
+            number++;
+        }
+    }
+
+    @Test
+    void testInstructionFetchRunOnBranch() {
+    }
 }
