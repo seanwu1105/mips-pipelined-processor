40e7bb957a57f2f8e4cc3c732763b037d240c25d
Sean Wu <seanwu1105@gmail.com>
2020-05-01 11:53:19 +0800

    Implement WriteBack class.

--- src/main/java/component/pipeline/WriteBack.java
+++ src/main/java/component/pipeline/WriteBack.java
@@ -1,12 +1,33 @@
 package component.pipeline;
 
+import component.Register;
+import controller.MainController;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
 public class WriteBack implements Stage {
 
-    public WriteBack(MemoryAccessToWriteBackRegister memWb) {
+    @NotNull
+    private final MemoryAccessToWriteBackRegister memWb;
+
+    @NotNull
+    private final Register register;
+
+    public WriteBack(@NotNull MemoryAccessToWriteBackRegister memWb, @NotNull Register register) {
+        this.memWb = memWb;
+        this.register = register;
     }
 
     @Override
     public void run() {
+        register.setRegisterWrite(Objects.requireNonNull(memWb.getRegisterWrite()));
+        if (memWb.getRegisterWrite() == MainController.RegisterWrite.TRUE)
+            register.setWriteAddress(memWb.getWriteRegisterAddress());
 
+        if (memWb.getMemoryToRegister() == MainController.MemoryToRegister.FROM_ALU_RESULT)
+            register.write(memWb.getAluResult());
+        else if (memWb.getMemoryToRegister() == MainController.MemoryToRegister.FROM_MEMORY)
+            register.write(memWb.getMemoryReadData());
     }
 }
